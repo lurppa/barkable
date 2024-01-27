a66f4ff40242bd3e1ae47825c76aa1978a8d50a8
Lari Nurkka <lari.nurkka@gmail.com>
2024-01-27 14:47:21 +0200

    Throw items around the player instead of right at it

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -4,6 +4,7 @@ signal item_hit_player(item)
 
 const TARGET_OFFSET = Vector3(0, 10, 0)
 const THROW_VELOCITY = 10.0
+const THROW_X_RANDOMNESS = 1.0
 
 @export_node_path var player_path
 @export_node_path var throw_point1_path
@@ -21,7 +22,6 @@ func throw_multiple_items():
 
 func start_throwing(count_of_items: int):
 	for i in range(count_of_items):
-		print(str(i) + ' toss')
 		get_tree().create_timer(i/2.0).connect("timeout", throw_item)
 
 
@@ -29,8 +29,9 @@ func throw_item():
 	# Get a random point between throw points
 	var throw_point = throw_point1.position.lerp(throw_point2.position, randf())
 
-	# Get a normalized vector direction from chosen point to player
-	var dir_to_player = (player.position + TARGET_OFFSET - throw_point).normalized()
+	# Get a normalized vector direction from chosen point to around the player
+	var horizontal_offset = Vector3(THROW_X_RANDOMNESS * 2 * randf() - THROW_X_RANDOMNESS, 0, 0)
+	var dir_to_player = (player.position + horizontal_offset + TARGET_OFFSET - throw_point).normalized()
 
 	# Instantiate a random item with values calculated earlier
 	var item = throwable_items[randf() * len(throwable_items)].instantiate()
